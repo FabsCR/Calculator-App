4d4caa11faa9fa5f204d2c949a235e242f16c798
FabsCR <fabfernandez@estudiantec.cr>
2022-11-20 00:00:00 -0600

    fixing bugs

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+// Function to add parameter value
 function numeros(valor){
-    document.getElementById('result').value += valor;
+    document.getElementById('result').value += valor; 
 }
 
+// Function to evaluate the operation
 function operacion(){
     var opera = document.getElementById('result').value;
     if(opera == 0){
-        document.getElementById('result').value = "Sin operacion"
+        document.getElementById('result').value = "No operation"
     } else{
         document.getElementById('result').value = eval(opera);
     }
 }
 
+// Function to reset the screen of the calculator
 function reset(){
     document.getElementById('result').value = " ";
 }
